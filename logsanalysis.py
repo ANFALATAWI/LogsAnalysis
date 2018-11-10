c2712d1b129997b5cf353454e6576452badbb5f0
Anfal <anfal.alatawi@gmail.com>
2018-11-11 00:08:54 +0300

    restrecting run to terminal

--- logsanalysis.py
+++ logsanalysis.py
@@ -2,78 +2,70 @@
 
 import psycopg2
 
-DBNAME = "news"
 
-# Connecting to the database
-database = psycopg2.connect(database=DBNAME)
-
-# Getting a cursor
-cursor = database.cursor()
-
-# ----------------------------
-# Query 1
-# This query uses VIEW articleLogs
-query = ("SELECT title, count(articleLogs.replace) as views "
-         "FROM articles, articleLogs WHERE slug = replace "
-         "GROUP BY title "
-         "ORDER BY views DESC "
-         "LIMIT 3; ")
-
-# Executing Query 1
-cursor.execute(query)
-
-# Fetching results
-results = cursor.fetchall()
-
-# Printing results
-print "1. What are the most popular three articles of all time?"
-for row in results:
-    sentence = "Article titled '{}' has {} views.".format(row[0], row[1])
-    print sentence
+def answer(query):
+        # Executing Query
+    cursor.execute(query)
+    # Fetching results
+    return cursor.fetchall()
 
 
-# ----------------------------
-# Query 2
-query = ("SELECT name, count(articleLogs.replace) as views "
-         "FROM articles, articleLogs, authors "
-         "WHERE slug = replace and authors.id = articles.author "
-         "GROUP BY name "
-         "ORDER BY views desc;")
+if __name__ == "__main__":
+    DBNAME = "news"
 
-# Executing Query 2
-cursor.execute(query)
+    # Connecting to the database
+    database = psycopg2.connect(database=DBNAME)
 
-# Fetching results
-results = cursor.fetchall()
+    # Getting a cursor
+    cursor = database.cursor()
 
-# Printing results
-print "\n2. Who are the most popular article authors of all time?"
-for row in results:
-    sentence = "Author '{}' has {} views.".format(row[0], row[1])
-    print sentence
+    # ----------------------------
+    # Query 1
+    # This query uses VIEW articleLogs
+    query = ("SELECT title, count(articleLogs.replace) as views "
+             "FROM articles, articleLogs WHERE slug = replace "
+             "GROUP BY title "
+             "ORDER BY views DESC "
+             "LIMIT 3; ")
 
-# ----------------------------
-# Query 3
-# This query uses VIEW dayStatus
-# Casting to floast was used to get an arethmatic value
-query = ("SELECT day, "
-         "((faliure::float/ (success::float+faliure::float))*100 )"
-         " as percentage "
-         "FROM dayStatus "
-         "WHERE ( ( (success+faliure) * 0.01) <= faliure);")
+    results = answer(query)
 
-# Executing Query 3
-cursor.execute(query)
+    # Printing results
+    print "1. What are the most popular three articles of all time?"
+    for row in results:
+        sentence = "Article titled '{}' has {} views.".format(row[0], row[1])
+        print sentence
 
-# Fetching results
-results = cursor.fetchall()
+    # ----------------------------
+    # Query 2
+    results = answer("SELECT name, count(articleLogs.replace) as views "
+                     "FROM articles, articleLogs, authors "
+                     "WHERE slug = replace and authors.id = articles.author "
+                     "GROUP BY name "
+                     "ORDER BY views desc;")
 
-# Printing results
-print "\n3. On which days did more than 1% of requests lead to errors?"
-for row in results:
-    sentence = "Day {} had {:0.2f}% of requests lead to errors.".format(
-        row[0], row[1])
-    print sentence
+    # Printing results
+    print "\n2. Who are the most popular article authors of all time?"
+    for row in results:
+        sentence = "Author '{}' has {} views.".format(row[0], row[1])
+        print sentence
 
-# Closing the database
-database.close()
+    # ----------------------------
+    # Query 3
+    # This query uses VIEW dayStatus
+    # Casting to floast was used to get an arethmatic value
+    results = answer("SELECT day, "
+                     "((faliure::float/ (success::float+faliure::float))*100 )"
+                     " as percentage "
+                     "FROM dayStatus "
+                     "WHERE ( ( (success+faliure) * 0.01) <= faliure);")
+
+    # Printing results
+    print "\n3. On which days did more than 1% of requests lead to errors?"
+    for row in results:
+        sentence = "Day {} had {:0.2f}% of requests lead to errors.".format(
+            row[0], row[1])
+        print sentence
+
+    # Closing the database
+    database.close()
