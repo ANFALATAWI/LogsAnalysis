8a49f721dac81f6f9af36f4f9b8062a415586cd9
Anfal <anfal.alatawi@gmail.com>
2018-11-10 23:49:18 +0300

    convert to PEP8 style

--- logsanalysis.py
+++ logsanalysis.py
@@ -5,16 +5,22 @@ import psycopg2
 DBNAME = "news"
 
 # Connecting to the database
-database = psycopg2.connect(database = DBNAME)
+database = psycopg2.connect(database=DBNAME)
 
 # Getting a cursor
-cursor  = database.cursor()
+cursor = database.cursor()
 
 # ----------------------------
 # Query 1
-# Executing Query 1
 # This query uses VIEW articleLogs
-cursor.execute("SELECT title, count(articleLogs.replace) as views FROM articles, articleLogs WHERE slug = replace GROUP BY title ORDER BY views DESC LIMIT 3")
+query = ("SELECT title, count(articleLogs.replace) as views "
+         "FROM articles, articleLogs WHERE slug = replace "
+         "GROUP BY title "
+         "ORDER BY views DESC "
+         "LIMIT 3; ")
+
+# Executing Query 1
+cursor.execute(query)
 
 # Fetching results
 results = cursor.fetchall()
@@ -22,15 +28,20 @@ results = cursor.fetchall()
 # Printing results
 print "1. What are the most popular three articles of all time?"
 for row in results:
-	sentence = "Article titled '{}' has {} views.".format(row[0], row[1]) 
-	print sentence
+    sentence = "Article titled '{}' has {} views.".format(row[0], row[1])
+    print sentence
 
 
 # ----------------------------
 # Query 2
+query = ("SELECT name, count(articleLogs.replace) as views "
+         "FROM articles, articleLogs, authors "
+         "WHERE slug = replace and authors.id = articles.author "
+         "GROUP BY name "
+         "ORDER BY views desc;")
 
 # Executing Query 2
-cursor.execute("SELECT name, count(articleLogs.replace) as views FROM articles, articleLogs, authors WHERE slug = replace and authors.id = articles.author GROUP BY name ORDER BY views desc;")
+cursor.execute(query)
 
 # Fetching results
 results = cursor.fetchall()
@@ -38,15 +49,21 @@ results = cursor.fetchall()
 # Printing results
 print "\n2. Who are the most popular article authors of all time?"
 for row in results:
-	sentence = "Author '{}' has {} views.".format(row[0], row[1]) 
-	print sentence
+    sentence = "Author '{}' has {} views.".format(row[0], row[1])
+    print sentence
 
 # ----------------------------
 # Query 3
-# Executing Query 3
 # This query uses VIEW dayStatus
 # Casting to floast was used to get an arethmatic value
-cursor.execute("SELECT day, (   ( faliure::float / (success::float + faliure::float) ) *100 ) as percentage FROM dayStatus WHERE ( ( (success+faliure) * 0.01) <= faliure);")
+query = ("SELECT day, "
+         "((faliure::float/ (success::float+faliure::float))*100 )"
+         " as percentage "
+         "FROM dayStatus "
+         "WHERE ( ( (success+faliure) * 0.01) <= faliure);")
+
+# Executing Query 3
+cursor.execute(query)
 
 # Fetching results
 results = cursor.fetchall()
@@ -54,8 +71,9 @@ results = cursor.fetchall()
 # Printing results
 print "\n3. On which days did more than 1% of requests lead to errors?"
 for row in results:
-	sentence = "Day {} had {:0.2f}% of requests lead to errors.".format(row[0], row[1]) 
-	print sentence
+    sentence = "Day {} had {:0.2f}% of requests lead to errors.".format(
+        row[0], row[1])
+    print sentence
 
 # Closing the database
 database.close()
